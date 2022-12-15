47d2994c26544a2f6993d18920f67a62278a8ff4
Satoe Sakonjo <sakonjo310@gmail.com>
2022-12-14 20:46:42 -0800

    Improve login page style with Bootstrap

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -14,6 +14,7 @@ class SessionsController < ApplicationController
     else
     # If user's login doesn't work, send them back to the login form.
       redirect_to '/login'
+      flash[:message] = "Invalid email or password"
     end
   end
 
