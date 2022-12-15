3d0546614a949fdc69a1481e202addbe87ddc960
Satoe Sakonjo <sakonjo310@gmail.com>
2022-12-14 22:43:50 -0800

    Add validation to signup feature

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
 
   def new
+    @user = User.new
   end
 
   def create
@@ -9,8 +10,9 @@ class UsersController < ApplicationController
       session[:user_id] = @user.id
       redirect_to root_path
     else
-      redirect_to '/signup'
-      flash[:message] = "Missing Fields"
+      render :new
+      # redirect_to '/signup'
+      # flash[:message] = "Missing Fields"
     end
   end
 
