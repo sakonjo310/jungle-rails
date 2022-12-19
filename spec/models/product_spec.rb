a8d40e7639425742ce1b2e2f76768a646d490e74
Satoe Sakonjo <sakonjo310@gmail.com>
2022-12-19 12:51:20 -0800

    Write tests to validate users when signup and login

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -2,39 +2,41 @@ require 'rails_helper'
 
 RSpec.describe Product, type: :model do
   describe 'Validations' do
+    subject {
+      Product.create(
+        name: "Monsterra",
+        price: 40,
+        quantity: 20,
+        category: Category.create(name: "Wasabi")
+      )
+    }
 
     it 'is valid with valid attributes' do
-      @category = Category.create(name: "Wasabi")
-      product = Product.new(name: "Monsterra", price: 40, quantity: 20, category: @category)
-      expect(product).to be_valid
+      expect(subject).to be_valid
     end
 
     it 'is not valid without a name' do
-      @category = Category.create(name: "Wasabi")
-      product = Product.create(name: nil, price: 40, quantity: 20, category: @category)
-      expect(product).to_not be_valid
-      expect(product.errors.full_messages).to include("Name can't be blank")
+      subject.name = nil
+      expect(subject).to_not be_valid
+      expect(subject.errors.full_messages).to include("Name can't be blank")
     end
 
     it 'is not valid without a price' do
-      @category = Category.create(name: "Wasabi")
-      product = Product.create(name: "Monsterra", price: false, quantity: 20, category: @category)
-      expect(product).to_not be_valid
-      expect(product.errors.full_messages).to include("Price is not a number") 
+      subject.price = false
+      expect(subject).to_not be_valid
+      expect(subject.errors.full_messages).to include("Price is not a number") 
     end
 
     it 'is not valid without a quantity' do
-      @category = Category.create(name: "Wasabi")
-      product = Product.create(name: "Monsterra", price: 40, quantity: nil, category: @category)
-      expect(product).to_not be_valid
-      expect(product.errors.full_messages).to include("Quantity can't be blank")
+      subject.quantity = nil
+      expect(subject).to_not be_valid
+      expect(subject.errors.full_messages).to include("Quantity can't be blank")
     end
 
     it 'is not valid without a category' do
-      @category = Category.create(name: "Wasabi")
-      product = Product.create(name: "Monsterra", price: 40, quantity: 20, category: nil)
-      expect(product).to_not be_valid
-      expect(product.errors.full_messages).to include("Category must exist")
+      subject.category = nil
+      expect(subject).to_not be_valid
+      expect(subject.errors.full_messages).to include("Category must exist")
     end
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,100 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  it ''
+  subject {
+    User.create(
+      first_name: 'Satoe',
+      last_name: 'Sakonjo',
+      email: 'test@test.com',
+      password: '123',
+      password_confirmation: '123'
+    )
+  }
+
+  describe 'Validations' do
+    it 'is valid with valid attributes' do
+      expect(subject).to be_valid
+    end
+
+    it 'is not valid without first_name' do
+      subject.first_name = nil
+      expect(subject).to_not be_valid
+    end
+
+    it 'is not valid without last_name' do
+      subject.last_name = nil
+      expect(subject).to_not be_valid
+    end
+
+    it 'is not valid without email' do
+      subject.email = nil
+      expect(subject).to_not be_valid
+    end
+
+    it 'is not valid without password' do
+      subject.password = nil
+      expect(subject).to_not be_valid
+    end
+
+    it 'is not valid without password confirmation' do
+      subject.password_confirmation = nil
+      expect(subject).to_not be_valid
+    end
+
+    it 'is not valid with password confirmation does not match the password' do
+      subject.password_confirmation = '234'
+      expect(subject).to_not be_valid
+    end
+
+    it 'is not valid with password less than 3 characters length' do
+      subject.password = '12'
+      subject.password_confirmation = '12'
+      expect(subject).to_not be_valid
+    end
+
+    it 'is not valid without unique email' do
+      subject.save
+      @user = User.create(
+        first_name: 'Satoe',
+        last_name: 'Sakonjo',
+        email: 'TEST@test.com',
+        password: '123',
+        password_confirmation: '123'
+      )
+      expect(@user).to_not be_valid
+    end
+  end
+
+  describe '.authenticate_with_credentials' do
+    it 'is valid with the valid credentials' do
+      subject.save
+      @user = User.authenticate_with_credentials('test@test.com', '123')
+      expect(@user).to_not be_nil
+    end
+
+    it 'is not valid without valid email' do
+      subject.save
+      @user = User.authenticate_with_credentials('test2@test.com', '123')
+      expect(@user).to be_nil
+    end
+
+    it 'is not valid without valid password' do
+      subject.save
+      @user = User.authenticate_with_credentials('test@test.com', '1234')
+      expect(@user).to be_nil
+    end
+
+    it 'is valid without valid email with spaces around it' do
+      subject.save
+      @user = User.authenticate_with_credentials(' test@test.com ', '123')
+      expect(@user).to_not be_nil
+    end
+
+    it 'is valid without valid email with wrong case' do
+      subject.save
+      @user = User.authenticate_with_credentials('TEST@test.com', '123')
+      expect(@user).to_not be_nil
+    end
+
+  end
 end
