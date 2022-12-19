6eaf23e278d5b41c39651b21f3bebd57f0334135
Satoe Sakonjo <sakonjo310@gmail.com>
2022-12-18 20:34:57 -0800

    Set up RSpect

--- config/routes.rb
+++ config/routes.rb
@@ -20,14 +20,6 @@ Rails.application.routes.draw do
     resources :categories, except: [:edit, :update, :show]
   end
 
-  # get '/signup' => 'users#new'
-  # post '/users' => 'users#create'
-  
-  # get '/login' => 'sessions#new'
-  # post '/login' => 'sessions#create'
-  
-  # delete '/logout' => 'sessions#destroy'
-
   get 'users/new' => 'users#new', as: :new_user
 	post 'users' => 'users#create'
 
