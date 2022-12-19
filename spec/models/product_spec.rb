2c318c61a1dbf5426f597458365e72272ec2906f
Satoe Sakonjo <sakonjo310@gmail.com>
2022-12-19 11:38:24 -0800

    Refactor product test and set up user test

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -2,32 +2,37 @@ require 'rails_helper'
 
 RSpec.describe Product, type: :model do
   describe 'Validations' do
-  
+
     it 'is valid with valid attributes' do
-      product = Product.new(name: "Monsterra", price: 40, quantity: 20, category: Category.new)
+      @category = Category.create(name: "Wasabi")
+      product = Product.new(name: "Monsterra", price: 40, quantity: 20, category: @category)
       expect(product).to be_valid
     end
 
     it 'is not valid without a name' do
-      product = Product.new(name: nil, price: 40, quantity: 20, category: Category.new)
+      @category = Category.create(name: "Wasabi")
+      product = Product.create(name: nil, price: 40, quantity: 20, category: @category)
       expect(product).to_not be_valid
       expect(product.errors.full_messages).to include("Name can't be blank")
     end
 
     it 'is not valid without a price' do
-      product = Product.new(name: "Monsterra", price: false, quantity: 20, category: Category.new)
+      @category = Category.create(name: "Wasabi")
+      product = Product.create(name: "Monsterra", price: false, quantity: 20, category: @category)
       expect(product).to_not be_valid
       expect(product.errors.full_messages).to include("Price is not a number") 
     end
 
     it 'is not valid without a quantity' do
-      product = Product.new(name: "Monsterra", price: 40, quantity: nil, category: Category.new)
+      @category = Category.create(name: "Wasabi")
+      product = Product.create(name: "Monsterra", price: 40, quantity: nil, category: @category)
       expect(product).to_not be_valid
       expect(product.errors.full_messages).to include("Quantity can't be blank")
     end
 
     it 'is not valid without a category' do
-      product = Product.new(name: "Monsterra", price: 40, quantity: 20, category: nil)
+      @category = Category.create(name: "Wasabi")
+      product = Product.create(name: "Monsterra", price: 40, quantity: 20, category: nil)
       expect(product).to_not be_valid
       expect(product.errors.full_messages).to include("Category must exist")
     end
